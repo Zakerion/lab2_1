9a035b1ac74744b2931c5b1d9f5180957a83ed05
Przyjemski.Maciej <180264@edu.p.lodz.pl>
2017-03-21 13:28:04 +0100

    testy_v1 dodanie testu isInSeq

---
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -0,0 +1,17 @@
+package edu.iis.mto.bsearch;
+
+import static org.junit.Assert.*;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+public class BinarySearchTest {
+
+	@Test
+	public void isInSeq() {
+		int[] seq = new int[] { 5 };
+		SearchResult result = BinarySearch.search(5, seq);
+		assertThat(result.isFound(), Matchers.equalTo(true));
+	}
+
+}
